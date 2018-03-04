ef992d8d65fe9979e1e40738b21eae64678caf1e
Pengdi Xia <xpd50@cs.utexas.edu>
2018-03-04 17:12:06 -0600

    no idea f**k that

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -5,16 +5,54 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void halt (void);
+static void exit (int status);
+static pid_t exec (const char *cmd_line);
+static int wait (pid_t pid);
+static bool create (const char *file, unsigned initial_size);
+static bool remove (const char *file);
+static int open (const char *file);
+static int filesize (int fd);
+static int read (int fd, void *buffer, unsigned size);
+static int write (int fd, const void *buffer, unsigned size);
+static void seek (int fd, unsigned position);
+static unsigned tell (int fd);
+static void close (int fd);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f)
 {
   printf ("system call!\n");
+  switch (f->esp) {
+    //the exec and wait
+    case SYS_EXEC:
+      temp=f->esp+1;
+      exec((const char*) temp);
+
+    case SYS_WAIT:
+    //the the file operating
+    case SYS_CREATE:
+    case SYS_REMOVE:
+    case SYS_OPEN:
+    case SYS_FILESIZE:
+    case SYS_READ:
+    case SYS_WRITE:
+    case SYS_SEEK:
+    case SYS_TELL:
+    case SYS_CLOSE:
+    //other sys call
+    case SYS_HALT:
+    case SYS_EXIT:
+  }
   thread_exit ();
+}
+
+static pid_t exec (const char *cmd_line){
+  return process_execute(cmd_line);
 }
